dd1b032ebfee5567c65d5a41a7f3f1330b3c1fb9
Muhammad Bilal <muhbil171845@gmail.com>
2024-12-10 12:23:45 +0500

    added methods for trending posts and liked posts

--- PostManager.cpp
+++ PostManager.cpp
@@ -31,3 +31,31 @@ vector<Post*> PostManager::getAllPosts() const {
     }
     return posts;
 }
+
+bool PostManager::likePost(int postId) {
+    PostNode* current = head;
+    while (current) {
+        if (current->post->postId == postId) {
+            current->post->likePost();
+            cout << "Post with ID " << postId << " liked successfully!" << endl;
+            return true;
+        }
+        current = current->next;
+    }
+    cout << "Post with ID " << postId << " not found!" << endl;
+    return false;
+}
+
+vector<Post*> PostManager::viewTrendingPosts() const {
+    vector<Post*> posts = getAllPosts();
+    
+    for (size_t i = 0; i < posts.size() - 1; i++) {
+        for (size_t j = 0; j < posts.size() - i - 1; j++) {
+            if (posts[j]->likes < posts[j + 1]->likes) {
+                swap(posts[j], posts[j + 1]);
+            }
+        }
+    }
+
+    return posts;
+}
