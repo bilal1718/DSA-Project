cdcea0c7e3fb70924796564e38295ac92fd47ad3
Muhammad Bilal <muhbil171845@gmail.com>
2024-12-10 12:22:02 +0500

    Integrated like post and trending posts

--- main.cpp
+++ main.cpp
@@ -1,26 +1,28 @@
 #include <iostream>
-#include <string>
+#include <fstream>
 #include <vector>
 #include "User.h"
 #include "PostManager.h"
-
 using namespace std;
 
-void showMenu() {
-    cout << "1. Register\n";
-    cout << "2. Login\n";
-    cout << "3. Exit\n";
-    cout << "Choose an option: ";
-}
+
 
 void showUserMenu() {
     cout << "\n--- User Menu ---\n";
     cout << "1. Create Post\n";
     cout << "2. View All Posts\n";
-    cout << "3. Logout\n";
+    cout << "3. Like a Post\n";
+    cout << "4. View Trending Posts\n";
+    cout << "5. Logout\n";
     cout << "Choose an option: ";
 }
-
+void showMenu() {
+    cout << "\n--- Main Menu ---\n";
+    cout << "1. Register\n";
+    cout << "2. Login\n";
+    cout << "3. Exit\n";
+    cout << "Choose an option: ";
+};
 int main() {
     User::loadUsersFromFile();
 
@@ -80,6 +82,20 @@ int main() {
                     cout << "Likes: " << post->likes << "\n\n";
                 }
             } else if (choice == 3) {
+                int postId;
+                cout << "Enter Post ID to like: ";
+                cin >> postId;
+                postManager.likePost(postId);
+            } else if (choice == 4) {
+                vector<Post*> trendingPosts = postManager.viewTrendingPosts();
+                cout << "\n--- Trending Posts ---\n";
+                for (const auto& post : trendingPosts) {
+                    cout << "Post ID: " << post->postId << "\n";
+                    cout << "Author: " << post->author << "\n";
+                    cout << "Content: " << post->content << "\n";
+                    cout << "Likes: " << post->likes << "\n\n";
+                }
+            } else if (choice == 5) {
                 loggedInUser = nullptr;
                 cout << "Logged out successfully.\n";
             } else {
