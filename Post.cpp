bf41fff64f7628bda6eb8723a0fb1f0d982b5c6a
Muhammad Bilal <muhbil171845@gmail.com>
2024-12-21 19:58:41 +0500

    Delete Post.cpp

--- Post.cpp
+++
@@ -1,8 +0,0 @@
-#include "Post.h"
-
-Post::Post(int postId, const string& content, const string& author)
-    : postId(postId), content(content), author(author), likes(0) {}
-
-void Post::likePost() {
-    likes++;
-}
